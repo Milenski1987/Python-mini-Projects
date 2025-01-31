0fcdd0236be8b27113fe3ab640dcf91217831eba
Milen Nikolov <milenski1987@icloud.com>
2025-02-01 00:41:38 +0200

    added some colors

--- countdown_timer.py
+++ countdown_timer.py
@@ -1,15 +1,16 @@
 import time
+from colorama import Fore, Back
 
 def countdown(t):
 
     while t >= 0:
         mins, secs = divmod(t, 60)
         timer = f'{mins:02d}:{secs:02d}'
-        print(f"\r{timer}", end="")
+        print(Back.BLACK + Fore.MAGENTA + f"\r{timer}", end="")
         time.sleep(1)
         t -= 1
     print(f"\nTime is over")
 
-t = int(input("Enter time in seconds: "))
+t = int(input(Fore.CYAN + "Enter time in seconds: "))
 
 countdown(t)
