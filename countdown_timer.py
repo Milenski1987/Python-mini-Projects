68afd3b62a6f9ce03cebc3a0796d64de738a31d5
Milen Nikolov <milenski1987@icloud.com>
2025-02-01 16:12:17 +0200

    final update for countdown_timer

--- countdown_timer.py
+++ countdown_timer.py
@@ -1,16 +1,23 @@
 import time
 from colorama import Fore, Back
 
+
 def countdown(t: int) -> str:
-    while t >= 0:
-        minutes, seconds = divmod(t, 60)
-        timer = f'{minutes:02d}:{seconds:02d}'
-        print(Back.BLACK + Fore.MAGENTA + f"\r{timer}", end="")
+    minutes, seconds = divmod(t, 60)
+    timer = f'{minutes:02d}:{seconds:02d}'
+    return f"\r{timer}"
+
+
+def main():
+    time_in_seconds = int(input(Fore.CYAN + "Enter time in seconds: "))
+
+    while time_in_seconds >= 0:
+        print(Back.BLACK + Fore.MAGENTA + f"\r{countdown(time_in_seconds)}", end = "")
         time.sleep(1)
-        t -= 1
-    return f"\nTime is over"
+        time_in_seconds -= 1
 
-time_in_seconds = int(input(Fore.CYAN + "Enter time in seconds: "))
 
-print(countdown(time_in_seconds))
+main()
+print()
+print("Time is over!!!")
 
